ab720119dc50d376bb03fe60c83320587694cb04
muysengly-itc <muy.sengly@itc.edu.kh>
2026-02-09 17:12:02 +0700

    update

---
+++ server/Docker.sh
@@ -0,0 +1,8 @@
+docker build -t gtr_telegram_product:latest -f server/services/telegram_bot/product/Dockerfile .
+
+
+CONTAINER_NAME="gtr_telegram_product"
+docker run -d \
+    --restart unless-stopped \
+    --name $CONTAINER_NAME \
+    gtr_telegram_product:latest
